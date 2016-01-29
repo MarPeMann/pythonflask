164566a81fd3381a4f4b5d0b6e3709d12d0b7ebe
MarPeMann <mpetma@hotmail.com>
2016-01-29 14:46:20 +0200

    db stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 #define the configure file of the application
 app.config.from_object('config')
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 from app import routers
--- app/routers.py
+++ app/routers.py
@@ -1,12 +1,28 @@
 from app import app
 #render_template provides access to jinja2 template engine
-from flask import render_template,request,make_response
+from flask import render_template,request,make_response,flash,redirect
 from app.forms import LoginForm
+from app.forms import RegisterForm
+from app.db_models import Users
+from app import db
 
 @app.route('/',methods=['GET','POST'])
 def index():
 	login = LoginForm()
-	return render_template('template_index.html',form=login)
+	#check if get method
+	if request.method == 'GET':
+		return render_template('template_index.html',form=login)
+	else:
+		#check if form data is valid
+		if login.validate_on_submit():
+			print(login.email.data)
+			print(login.passw.data)
+			return render_template('template_user.html')
+		#form data was not valid
+		else:
+			flash('Incorrect credentials')
+			return render_template('template_index.html',form=login)
+
 
 @app.route('/user/<name>')
 def user(name):
@@ -18,4 +34,21 @@ def user(name):
 def userParams():
 	name = request.args.get('name')
 	return render_template('template_user.html',name=name)
+
+@app.route('/register',methods=['GET','POST'])
+def registerUser():
+	form = RegisterForm()
+	if request.method == 'GET':
+		return render_template('template_register.html',form=form)
+	else:
+		if form.validate_on_submit():
+			user = Users(form.email.data, form.passw.data)
+			db.session.add(user)
+			db.session.commit()
+			flash("Name {0} registered".format(form.email.data).format("hei"))
+			return redirect('/')
+		else:
+			flash('Fill in the required fields')
+			return render_template('template_register.html',form=form)
+
 
